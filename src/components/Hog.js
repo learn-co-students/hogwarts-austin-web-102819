7b965f9c00da54bb3975040eed7c7182bd68862f
Buford Eeds <bufordeeds8@gmail.com>
2020-01-09 14:00:54 -0600

    working

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -2,15 +2,31 @@ import React, { Component } from "react";
 // import hogs from "../porkers_data";
 
 export class Hog extends Component {
-  const = {name}
+  // const = {name}
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+  }
+
+  handleDetailsClick = () => {
+    //when clicked, change clicked to true
+    this.setState({ clicked: !this.state.clicked });
+  };
+
   render() {
+
+    
     return (
       <div className="ui card eight wide column pigTile">
-        <p>{this.props.name}</p>
-        <p>{this.props.specialty}</p>
-        <p>{this.props.weight}</p> 
-        <p>{this.props.greased}</p>
-       
+        <h3>{this.props.name}</h3>
+        
+        <div>
+          <button className='ui button' onClick={this.handleDetailsClick}>
+          {this.state.clicked ? "Less Info" : "More Info"}
+          </button>
+        </div>
       </div>
     );
   }
